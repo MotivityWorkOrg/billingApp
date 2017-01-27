59c482c71275c933a47390a51b4eb5355173d5c5
damarlaravi <damarlaravi@gmail.com>
2017-01-27 09:21:11 +0530

    Fixed discount issue

--- frontend/directives/discount.directive.js
+++ frontend/directives/discount.directive.js
@@ -6,20 +6,31 @@ function DiscountDirective($log) {
         link: function (scope, ele) {
             $log.log('In Discount Directive');
             let homeController = scope.home;
+            let oldPrice = homeController.orderPrice;
+            let totalOrderPrice = Number(homeController.orderPrice);
+            ele.bind('click focus' , function () {
+                oldPrice = homeController.orderPrice;
+                totalOrderPrice = Number(homeController.orderPrice);
+            });
             ele.bind("keyup", (event) => {
                 if ((event.keyCode > 47 && event.keyCode < 58) || (event.keyCode > 95 && event.keyCode < 104) || (event.keyCode === 8 || event.keyCode === 127 )) {
-                    let discount = event.currentTarget.value;
+                    //let oldPrice = homeController.orderPrice;
+                    let discount = event.currentTarget.value !== "" ? parseInt(event.currentTarget.value) : 0.0;
+
                     let discountTotal = 0.0;
-                    let oderTotalPrice = 0.0;
-                    if(event.currentTarget.value > 0){
-                        discountTotal = (discount * Number(homeController.orderPrice)) / 100;
-                        oderTotalPrice = homeController.orderPrice - discountTotal;
-                        oderTotalPrice = Math.round(oderTotalPrice);
-                        scope.$apply(() => {
-                            homeController.orderPrice = oderTotalPrice.toFixed(2);
-                            homeController.itemInCart = false;
-                        });
+                    let revisedTotalPrice = 0.0;
+                    if(discount > 0){
+                        discountTotal = (discount * totalOrderPrice) / 100;
+                        let afterDiscTotal = (totalOrderPrice - discountTotal);
+                        revisedTotalPrice = Math.round(afterDiscTotal);
                     }
+                    else {
+                        revisedTotalPrice = oldPrice;
+                    }
+                    scope.$apply(() => {
+                        homeController.orderPrice = revisedTotalPrice.toFixed(2);
+                        homeController.itemInCart = false;
+                    });
                 }
             });
         }
